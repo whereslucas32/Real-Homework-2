458a7911acfed63c21a8de67b2b36b3eee4cb212
Raelle Richard <doodlecat2000@yahoo.com>
2014-02-10 11:09:34 -0500

    Hopefully submitting some homework....

---
+++ Assets/Scripts/SphereCharacter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	public float turnSpeed = 3.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if (Input.GetAxis ("Mouse X") > 0) {
+			transform.Rotate(Vector3.up * turnSpeed);
+		}
+		if(Input.GetAxis("Mouse X") < 0) {
+			transform.Rotate(Vector3.up * -turnSpeed);
+		}
+
+	
+
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+				
+		}
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
